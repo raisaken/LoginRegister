5e9dba41c1de04e9e54ac0e466ef9cdf4ae1ace8
saken rai <saken.rai.34@gmail.com>
2022-03-24 17:45:58 +0545

    jquery

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const togglePassword = document.querySelector("#togglePassword");
 const password = document.querySelector("#password");
 
 togglePassword.addEventListener("click", function () {
-   
   // toggle the type attribute
-  const type = password.getAttribute("type") === "password" ? "text" : "password";
+  const type =
+  password.getAttribute("type") === "password" ? "text" : "password";
   password.setAttribute("type", type);
   // toggle the eye icon
-  this.classList.toggle('fa-eye');
-  this.classList.toggle('fa-eye-slash');
+  this.classList.toggle("fa-eye");
+  this.classList.toggle("fa-eye-slash");
+});
+
+$(".fb").hover(function () {
+  $(this).addClass("text-active");
+  $(this).removeClass("text-inactive");
+  $(".google").addClass("text-inactive");
+  $(".google").removeClass("text-active");
+});
+
+$(".google").hover(function () {
+  $(this).addClass("text-active");
+  $(this).removeClass("text-inactive");
+  $(".fb").addClass("text-inactive");
+  $(".fb").removeClass("text-active");
 });
